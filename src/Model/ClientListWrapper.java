df306899ba2a314f59f8c632dca505183d384156
Poulain17 <30335542+Poulain17@users.noreply.github.com>
2017-09-06 08:42:49 +0200

    Update ClientListWrapper.java

--- src/Model/ClientListWrapper.java
+++ src/Model/ClientListWrapper.java
@@ -1,5 +1,32 @@
 package Model;
 
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * Classe Wrap liste Client.
+ * Utiliser pour sauvegarder la liste de Client au format XML
+ * 
+ * @author Pauline G.
+ *
+ */
+
+@XmlRootElement(name = "Client")
+
 public class ClientListWrapper {
+	
+
+		private List<Client> client;
+		
+	/////////////////////////////// Getter/Setter \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\ 
+		
+		 @XmlElement(name = "client")
+		 public List<Client> getClient(){	 
+			 return client;}
+		 
+		 public void setClient(List<Client>client){
+			 this.client = client;}
 
 }
