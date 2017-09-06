ef0c6ae61525406ec0df8838bb62fc23139cf9c5
Poulain17 <30335542+Poulain17@users.noreply.github.com>
2017-09-06 08:48:13 +0200

    Update RepresentantListWrapper.java

--- src/Model/RepresentantListWrapper.java
+++ src/Model/RepresentantListWrapper.java
@@ -1,5 +1,30 @@
 package Model;
 
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+
+/**
+ * Classe qui  Wrap de la TypeBox. Sert a sauvegarder les donnée
+ * en XML
+ * @author Pauline G.
+ */
+
+@XmlRootElement(name = "Representant")
+
 public class RepresentantListWrapper {
+
+
+		private List<Representant> listRepresentant;
+		
+		@XmlElement(name = "NumVrp")
+		public List<Representant> getlistRepresentant(){
+			return listRepresentant;}
+		
+		public void setlistRepresentant(List<Representant>listTypeBox){
+			this.listRepresentant = listTypeBox;}
+
 
 }
